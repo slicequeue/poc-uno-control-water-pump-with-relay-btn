c0c4681540e2a32caf4afb768a25c078964c0940
slicequeue <slicequeue@gmail.com>
2024-12-22 15:04:22 +0900

    refac: 릴레이 관련 액티브 상수 설정 코드 개선, 주석 추가

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <Arduino.h>
 
-#define RELAY_PIN 7
 #define PUSH_BTN_PIN 12
+
+/*
+ * [JQC3F-05VDC-C 릴레이] 
+ * - 릴레이 모듈 Active LOW 이므로 다음과 같이 정의
+ */ 
+#define RELAY_PIN 7
+#define RELAY_ACTIVE LOW    // 릴레이 활성화 신호 정의
+#define RELAY_DEACTIVE HIGH // 릴레이 비활성화 신호 정의
 
 void setup() {
   Serial.begin(9600);
@@ -10,23 +17,15 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(RELAY_PIN, HIGH);
+  digitalWrite(RELAY_PIN, RELAY_DEACTIVE);
   int btnState = digitalRead(PUSH_BTN_PIN);
   Serial.print("btnState: ");
   Serial.println(btnState);
   if (btnState == LOW) { // 풀다운에 의한 버튼 눌릴 경우
     Serial.println("Button pushed! Relay ON!");
     // 3초 릴레이 가동 후 종료
-    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN, RELAY_ACTIVE);
     delay(3000);
   }
-  delay(1);
-
-  // [릴레이 단독 테스트 완료]
-  // Serial.println("Relay ON!");
-  // digitalWrite(RELAY_PIN, HIGH);
-  // delay(2000);
-  // Serial.println("Relay OFF!");
-  // digitalWrite(RELAY_PIN, LOW);
-  // delay(1000);
+  delay(1); // 버튼 입력을 감지하는 폴링 방식에서는 루프가 너무 빨리 도는 것이 시스템 자원 낭비 delay(1);으로 CPU를 쉬게 하는 것이 더 효율적
 }
